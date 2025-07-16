90222cc3fdfe0932a7b7cbb7da303dfb1e41e84e
rajendrachy <chyrajendra32@gmail.com>
2025-07-16 22:14:37 +0530

    upto 4 hour cpmplete 1 hour remaining

---
+++ Mongodb/PART4/logical_oper.js
@@ -0,0 +1,6 @@
+//------------Logical Operators--------------------
+// find products with price greater than 100 and name equal to "Diamond ring"
+
+// ------------done in MongoDb--------------
+
+
